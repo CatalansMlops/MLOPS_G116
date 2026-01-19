f2cca0c38bbb66343b82af59340c1582b368b40f
armandgatius <armandgatiusugalde@gmail.com>
2026-01-19 14:28:02 +0100

    dvc config

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,9 @@
 import os
 from PIL import Image
-from tests import _PATH_DATA 
+
+_TEST_ROOT = os.path.dirname(__file__)  # root of test folder
+_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)  # root of project
+_PATH_DATA = os.path.join(_PROJECT_ROOT, "data")  # root of data
 
 RAW_DIR = os.path.join(_PATH_DATA, "raw", "brain_dataset")
 
